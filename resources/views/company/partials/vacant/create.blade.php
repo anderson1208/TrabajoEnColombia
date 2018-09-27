65b02956d30c9fb7e49e65ccdcc78abad21338cf
jostyftm <stiwart94@gmail.com>
2018-09-27 04:36:33 -0500

    asas

--- resources/views/company/partials/vacant/create.blade.php
+++ resources/views/company/partials/vacant/create.blade.php
@@ -31,10 +31,5 @@
 @endsection
 
 @section('js')
-<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    <script src="/vendor/unisharp/laravel-ckeditor/adapters/jquery.js"></script>
-    <script>
-        $('textarea').ckeditor();
-        // $('.textarea').ckeditor(); // if class is prefered.
-    </script>
+	
 @endsection
